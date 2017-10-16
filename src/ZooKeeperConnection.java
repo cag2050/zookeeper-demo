8fc35e6d90c33cfb00587477b5ecf58aeba9bfd6
cag2050 <cag2050@163.com>
2017-10-16 18:49:57 +0800

    m,

--- src/ZooKeeperConnection.java
+++ src/ZooKeeperConnection.java
@@ -7,6 +7,8 @@ import java.util.concurrent.CountDownLatch;
 
 public class ZooKeeperConnection {
     private ZooKeeper zooKeeper;
+//    CountDownLatch是一个同步工具类，它允许一个或多个线程一直等待，直到其他线程的操作执行完后再执行。
+//    CountDownLatch是通过一个计数器来实现的，计数器的初始值为线程的数量。每当一个线程完成了自己的任务后，计数器的值就会减1。当计数器值到达0时，它表示所有的线程已经完成了任务，然后在闭锁上等待的线程就可以恢复执行任务。
     final CountDownLatch countDownLatch = new CountDownLatch(1);
 
     public ZooKeeper connect(String host) throws IOException, InterruptedException {
