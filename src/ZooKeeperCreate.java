0144ac5247021986b2c02ad85249e29fcc6dbfed
cag2050 <cag2050@163.com>
2017-10-17 10:06:37 +0800

    m

--- src/ZooKeeperCreate.java
+++ src/ZooKeeperCreate.java
@@ -5,7 +5,7 @@ import org.apache.zookeeper.ZooKeeper;
 
 public class ZooKeeperCreate {
     private static ZooKeeper zooKeeper;
-    private static ZooKeeperConnection zooKeeperConnection;
+    private static ZooKeeperConnectionHelper zooKeeperConnectionHelper;
 
     public static void create(String path, byte[] data) throws InterruptedException, KeeperException {
         zooKeeper.create(path, data, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
@@ -16,8 +16,8 @@ public class ZooKeeperCreate {
         byte[] data = "first znode app".getBytes();
 
         try {
-            zooKeeperConnection = new ZooKeeperConnection();
-            zooKeeper = zooKeeperConnection.connect("localhost");
+            zooKeeperConnectionHelper = new ZooKeeperConnectionHelper();
+            zooKeeper = zooKeeperConnectionHelper.connect("localhost");
             create(path, data);
             zooKeeper.close();
         } catch (Exception e) {
